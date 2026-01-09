1ff4fb99b7a2538817b739fb0639b84feeab384f
rbwls1001 <rbwls1001@naver.com>
2026-01-09 16:12:50 +0900

    Project initiation

---
+++ gamenews/src/test/java/com/kjlee/gamenews/GamenewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjlee.gamenews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamenewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
