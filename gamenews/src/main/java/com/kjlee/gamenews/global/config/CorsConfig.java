7b73a8e9231d4f5c6e7fc5c6b5135e4d294e64ef
rbwls1001 <rbwls1001@naver.com>
2026-02-27 15:43:57 +0900

    Fix: .gitignore 및 Dockerfile 수정, CORS 설정 오류 수정

--- gamenews/src/main/java/com/kjlee/gamenews/global/config/CorsConfig.java
+++ gamenews/src/main/java/com/kjlee/gamenews/global/config/CorsConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    @Value("{cors.allowed-origins")
+    @Value("${cors.allowed-origins}")
     private String[] allowedOrigins;
 
     @Override
